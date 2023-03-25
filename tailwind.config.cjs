decd706d1d2d2fa0da199e02848d2ca489654700
Piotr Filipiak <kiddhkane@gmail.com>
2023-03-25 23:54:51 +0100

    Added files and styles.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,33 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        tablet: "576px",
+        desktop: "1024px",
+      },
+      colors: {
+        lightRed: "hsl(0, 100%, 67%)",
+        orangeYellow: "hsl(39, 100%, 56%)",
+        greenTeal: "hsl(166, 100%, 37%)",
+        cobaltBlue: "hsl(234, 85%, 45%)",
+        // Gradients
+        // Background
+        lightSlateBlue: "hsl(252, 100%, 67%)",
+        lightRoyalBlue: "hsl(241, 81%, 54%)",
+        // Circle
+        violetBlue: "hsla(256, 72%, 46%, 1)",
+        persianBlue: "hsla(241, 72%, 46%, 0)",
+
+        // Neutral
+        paleBlue: "hsl(221, 100%, 96%)",
+        lightLavender: "hsl(241, 100%, 89%)",
+        darkGrayBlue: "hsl(224, 30%, 27%)",
+      },
+      fontFamily: {
+        hanken: ["Hanken Grotesk", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
