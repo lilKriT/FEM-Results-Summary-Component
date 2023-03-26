4d701571fcd8c702516e7e33ff14ee9d321d0c37
Piotr Filipiak <kiddhkane@gmail.com>
2023-03-26 22:46:11 +0200

    Styling.

--- src/Summary.tsx
+++ src/Summary.tsx
@@ -2,16 +2,22 @@ import React from "react";
 
 const Summary = () => {
   return (
-    <section className="bg-emerald-200 h-screen w-screen flex justify-center ">
+    <section className="bg-paleBlue h-screen w-screen flex justify-center ">
       <div>
         {/* Left Column */}
         <div className="bgGradient rounded-lg p-4">
           <h2>Your Result</h2>
-          76 of 100 Great You scored higher than 65% of the people who have
-          taken these tests.
+          <div className="circleGradient rounded-full">
+            <span className="text-3xl">76</span> of 100
+          </div>
+          Great You scored higher than 65% of the people who have taken these
+          tests.
         </div>
         {/* Right Column */}
-        <div></div>
+        <div className="bg-white">
+          Summary Reaction 80 / 100 Memory 92 / 100 Verbal 61 / 100 Visual 72 /
+          100 Continue
+        </div>
       </div>
     </section>
   );
