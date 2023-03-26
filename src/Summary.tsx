57fc8a6d3323a90335ab01e0c6a63e0b22bc4188
Piotr Filipiak <kiddhkane@gmail.com>
2023-03-26 22:48:42 +0200

    Styling.

--- src/Summary.tsx
+++ src/Summary.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Summary = () => {
   return (
-    <section className="bg-paleBlue h-screen w-screen flex justify-center ">
-      <div>
+    <section className="bg-paleBlue h-screen w-screen flex justify-center items-center ">
+      <div className="flex">
         {/* Left Column */}
-        <div className="bgGradient rounded-lg p-4">
+        <div className="w-1/2 bgGradient rounded-lg p-4">
           <h2>Your Result</h2>
           <div className="circleGradient rounded-full">
             <span className="text-3xl">76</span> of 100
@@ -14,9 +14,13 @@ const Summary = () => {
           tests.
         </div>
         {/* Right Column */}
-        <div className="bg-white">
-          Summary Reaction 80 / 100 Memory 92 / 100 Verbal 61 / 100 Visual 72 /
-          100 Continue
+        <div className="w-1/2 bg-white">
+          <h2>Summary</h2>
+          <div> Reaction 80 / 100</div>
+          <div>Memory 92 / 100</div>
+          <div>Verbal 61 / 100</div>
+          <div>Visual 72 / 100</div>
+          <a href="#">Continue</a>
         </div>
       </div>
     </section>
